29932585f3b2f258bd66f7280af384f6fa0fe149
tintoll <belselios@gmail.com>
2018-05-17 17:26:42 +0900

    axios 기본 사용해보기

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
+import axios from 'axios';
 
 
 class App extends Component {
+
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
+             .then(res => console.log(res));
+    }
     render() {
         const { CounterActions, number } = this.props;
 
